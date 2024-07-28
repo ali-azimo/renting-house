77133286ff7d5346f5ec9f0d07087e229f44da35
Ali Azimo <webdesign.aejl@gmail.com>
2024-07-28 10:15:45 -0300

    errormunua

--- api/contrillers/authController.js
+++ api/contrillers/authController.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const singup = async(req, res) => {
+export const singup = async(req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
@@ -13,7 +14,7 @@ export const singup = async(req, res) => {
         await newUser.save();
         res.status(201).json('User created successfuly!');
     } catch (error) {
-        res.status(500).json(error.message);
+        next(errorHandler(550, "Error from the function"));
     }
 
 };
