bf2179aab0b03c8737b29b9fe087da598316a38d
Ali Azimo <webdesign.aejl@gmail.com>
2024-08-11 13:55:56 -0300

    update listing route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing, deleteListing } from '../contrillers/listingController.js';
+import { createListing, deleteListing, updateListing } from '../contrillers/listingController.js';
 const router = express.Router();
 
 
 
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing)
 export default router;
