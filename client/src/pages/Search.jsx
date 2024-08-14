39a6bcc493b096f4e5d5be82377b413be3955510
Ali Azimo <webdesign.aejl@gmail.com>
2024-08-14 12:24:44 -0300

    Search ui

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className='flex flex-col md:flex-row'> 
+        <div className="p-7 border-b-2 sm:border-r-2 md:min-h-screen">
+            <form className='flex flex-col gap-8'>
+                <div className="flex items-center gap-2">
+                    <label className='whitespace-nowrap font-semibold'>
+                        Search Term:</label>
+                        <input type="text" 
+                            id='searchTerm'
+                            placeholder='Search...'
+                            className='border rounded-lg p-3 w-full'
+                        />
+                </div>
+                <div className="flex gap-2 flex-wrap items-center">
+                    <label className='font-semibold'>Amnenities:</label>
+                    <div className="flex gap-2">
+                        <input type="checkbox" id="parking" className='w-5 font-semibold' />
+                        <span className='font-semibold'>Parking</span>
+                    </div>
+                    <div className="flex gap-2">
+                        <input type="checkbox" id="finishedt" className='w-5' />
+                        <span className='font-semibold'>Finished</span>
+                    </div>
+                    
+                </div>
+                <div className="flex items-center gap-2">
+                    <label className='font-semibold'>Sort:</label>
+                    <select id="sort_order"
+                        className='border rounded-lg p-2'
+                    >
+                        <option>Price high to low</option>
+                        <option>Price low to high</option>
+                        <option>Latest</option>
+                        <option>Oldest</option>
+                    </select>
+                </div>
+                <button className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>Search</button>
+            </form>
+        </div>
+        <div className="">
+            <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results</h1>
+        </div>
+    </div>
+  )
+}
